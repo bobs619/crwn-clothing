1214891ffb823026489b740f6153bc48f46ce175
Agonoy <BAgonoy@semprautilities.com>
2020-02-12 15:18:13 -0800

    Added header

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import  HomePage  from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import { Route, Switch } from 'react-router-dom';
+import Header from '../src/components/header/header';
 
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch> 
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
